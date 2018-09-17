d25f5b4a350cdefcfb55a223e46b9045e81d72dc
freemanjackal <maikel@plubr.com.br>
2018-09-17 17:06:13 -0300

    braziliex

---
+++ br.py
@@ -0,0 +1,143 @@
+"""
+The Bellman-Ford algorithm
+Graph API:
+    iter(graph) gives all nodes
+    iter(graph[u]) gives neighbours of u
+    graph[u][v] gives weight of edge (u, v)
+    
+props to: http://hetland.org/coding/python/bellman_ford.py
+"""
+from math import log
+import requests
+import json
+from binance.client import Client
+import threading
+
+"""
+headers= {'Accept': 'application/json', 'User-Agent': 'binance/python', 'X-MBX-APIKEY': '5ZskPFW3CeJEpbWItVTrPsy2ngSn3d1ued0cOH2kHipVuIhkMfETgxRN8Ljzrv9Q'}
+tickers = requests.get("https://api.binance.com/api/v1/ticker/24hr", headers=headers)
+"""
+
+client = Client('5ZskPFW3CeJEpbWItVTrPsy2ngSn3d1ued0cOH2kHipVuIhkMfETgxRN8Ljzrv9Q', 'tCsChD24BHmLjqSVcYFgY1TzPKLji9OAzVlJdQlSPhL7t2O8PDwWFhrkbhVqyuvA')
+
+def initialize(graph, source):
+    d = {}
+    p = {}
+    for node in graph:
+        d[node] = float('inf')
+        p[node] = None
+    d[source] = 0
+    return d, p
+
+def relax(u, v, graph, d, p):
+    try:
+        if d[v] > d[u] + graph[u][v]:
+            try:
+                d[v] = d[u] + graph[u][v]
+                p[v] = u
+            except:
+                pass
+    except:
+        pass
+
+def retrace_negative_loop(p, start, d):
+    arbitrageLoop = [start]
+    arbitragePrices = [d[start]]
+    next_node = start
+    try:
+        while True:
+            next_node = p[next_node]
+            if next_node not in arbitrageLoop:
+                arbitrageLoop.append(next_node)
+                arbitragePrices.append(d[next_node])
+            else:
+                arbitrageLoop.append(next_node)
+                arbitrageLoop = arbitrageLoop[arbitrageLoop.index(next_node):]
+                return arbitrageLoop, arbitragePrices
+    except Exception as e:
+        #print(e)
+        return None, None
+
+def bellman_ford(graph, source):
+    d, p = initialize(graph, source)
+    start = None
+
+    for i in range(len(graph)-1):
+        for u in graph:
+            for v in graph[u]:
+                relax(u, v, graph, d, p)
+    # one more relaxation to find negative weight cycle
+    print('ass')
+    print(graph)
+    for u in graph:
+        for v in graph[u]:
+            try:
+                if d[v] > d[u] + graph[u][v]:
+                    #relax(u, v, graph, d, p)
+                    start = u
+                    print('startttttttttttt ' + start)
+                    return retrace_negative_loop(p, start, d)
+                    #return d,p,start
+            except Exception as e:
+                pass
+    #return d,p,start
+    return None, None
+
+def test():
+    threading.Timer(50.0, test).start()
+
+    graph = {
+        'eth': {},
+        'btc': {},
+        'brl': {},
+        }
+    graphOriginal = {
+        'eth': {},
+        'btc': {},
+        'brl': {},
+        }
+    
+
+     
+    for v in graph.keys():
+        for u in graph.keys():
+            symb = str(v) + str(u)
+            try:
+                ticker = requests.get("https://braziliex.com/api/v1/public/ticker/" + v+"_"+u).json()
+                try:
+                    
+                        graph[v][u] = -log(float(ticker['highestBid']))
+                        graphOriginal[v][u] = float(ticker['highestBid'])
+                        graph[u][v] = -log(1/float(ticker['lowestAsk']))
+                        graphOriginal[u][v] = 1/float(ticker['lowestAsk'])
+
+
+                except  Exception as e:
+                    pass
+            except:
+                pass
+
+    print(graphOriginal)
+                #graph[v][u] = None
+    #d, p, _ = bellman_ford(graph, 'a')
+    res, pre = bellman_ford(graph, 'eth')
+
+    value = 1
+    valueO = value
+    if res != None:
+        for i in range(len(res), 1, -1):
+            pre = graphOriginal[res[i-1]][res[i-2]]
+            print(str(res[i-1])+str(res[i-2]))
+            value *= pre
+            #print("before fee--" + str(value))
+            value = value - value/100*0.4999
+            #print("after fee--" + str(value))
+            #print(value)
+
+    print("gains " + str(value - valueO))
+    if (value - valueO) > 0:
+        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+
+if __name__ == '__main__':
+     test()
